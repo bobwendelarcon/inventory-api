f9eb642e55b02b1b8101305b9f9cee5a8410dd59
bob arcon <bobwendelarcon@gmail.com>
2026-03-25 22:30:42 +0800

    Revert Program.cs to working Firebase file version

--- Program.cs
+++ Program.cs
@@ -8,27 +8,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-string firebaseJson = Environment.GetEnvironmentVariable("FIREBASE_KEY");
+string firebaseKeyPath = Environment.GetEnvironmentVariable("FIREBASE_KEY_PATH");
 
-if (string.IsNullOrEmpty(firebaseJson))
+if (string.IsNullOrEmpty(firebaseKeyPath))
 {
-    throw new Exception("FIREBASE_KEY environment variable is missing.");
+    firebaseKeyPath = "firebase-key.json";
 }
 
 if (FirebaseApp.DefaultInstance == null)
 {
     FirebaseApp.Create(new AppOptions
     {
-        Credential = GoogleCredential.FromJson(firebaseJson)
+        Credential = GoogleCredential.FromFile(firebaseKeyPath)
     });
 }
+
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", firebaseKeyPath);
+
 
 builder.Services.AddSingleton(provider =>
 {
     string projectId = builder.Configuration["Firebase:ProjectId"]!;
     return FirestoreDb.Create(projectId);
 });
-
 builder.Services.AddScoped<inventory_api.Services.CategoryService>();
 builder.Services.AddScoped<inventory_api.Services.ProductService>();
 builder.Services.AddScoped<inventory_api.Services.InventoryTransactionService>();
@@ -37,8 +39,8 @@ builder.Services.AddScoped<inventory_api.Services.ProductLotNumberService>();
 builder.Services.AddScoped<inventory_api.Services.UserService>();
 builder.Services.AddScoped<inventory_api.Services.PartnerService>();
 builder.Services.AddScoped<inventory_api.Services.InventoryDisplayService>();
-
 var app = builder.Build();
+
 
 app.UseSwagger();
 app.UseSwaggerUI();
